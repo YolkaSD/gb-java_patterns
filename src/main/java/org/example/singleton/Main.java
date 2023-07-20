cb018dccba3cd4f98f037dfea6ee91fa253b3d9e
yolkaSD <yolka.sd@gmail.com>
2023-07-20 21:11:20 +0700

    singleton

---
+++ src/main/java/org/example/singleton/Main.java
@@ -0,0 +1,8 @@
+package org.example.singleton;
+
+public class Main {
+    public static void main(String[] args) {
+        SimpleResource simpleResource = SimpleResource.getSimpleResource();
+
+    }
+}
---
+++ src/main/java/org/example/singleton/SimpleResource.java
@@ -0,0 +1,14 @@
+package org.example.singleton;
+
+public class SimpleResource {
+    private static SimpleResource simpleResource;
+
+    private SimpleResource(){}
+
+    public static SimpleResource getSimpleResource() {
+        if (simpleResource == null) {
+            simpleResource = new SimpleResource();
+        }
+        return simpleResource;
+    }
+}
